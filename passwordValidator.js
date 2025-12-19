3a351a3677c9d7222eee394f573cda038e965469
André <andre.pereira66170@gmail.com>
2025-12-19 10:57:45 +0100

    Fonctionalité : Ajout de la verifiaction d'au moins un chiffre dans le mot de passe et ajout des unitaire correspondant

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,7 +1,18 @@
 function validatePassword(password) {
     const minLength = 8;
+    const minNumberOfDigits = 1;
     if (password.length < minLength) {
         print(`Password must be at least ${minLength} characters long.`);
+        return false;
+    }
+    let digitCount = 0;
+    for (let char of password) {
+        if (char >= '0' && char <= '9') {
+            digitCount++;
+        }
+    }
+    if (digitCount < minNumberOfDigits) {
+        print(`Password must contain at least ${minNumberOfDigits} digit(s).`);
         return false;
     }
     return true;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,6 +1,8 @@
 const validatePassword = require('./passwordValidator');
 const wrongLengthPasswords = "a"*7;
-const validLengthPasswords = "a"*8;
+const validLengthPasswords = "aaaaaaa1";
+const wrongDigitPasswords = "abcdefgh";
+const validDigitPasswords = "abcd3fgh";
 
 test('password with less than 8 characters should be invalid', () => {
     const result = validatePassword(wrongLengthPasswords);
@@ -10,5 +12,16 @@ test('password with less than 8 characters should be invalid', () => {
 
 test('password with 8 or more characters should be valid', () => {
     const result = validatePassword(validLengthPasswords);
+    expect(result).toBe(true);
+});
+
+test('password with no digits should be invalid', () => {
+    const result = validatePassword(wrongDigitPasswords);
+    expect(result).toBe(false);
+}
+);
+
+test('password with at least one digit should be valid', () => {
+    const result = validatePassword(validDigitPasswords);
     expect(result).toBe(true);
 });
